afba3ffc5577a75e657de762b61124d1f9fda709
Sumanth <gopivenkata200@gmail.com>
2020-02-11 19:38:35 +0530

    Math_operations.java added to repostrie

---
+++ Math_operations.java
@@ -0,0 +1,54 @@
+package sample_calculator;
+
+public class Math_operations
+{
+ public static double add(double d,double e)
+ {
+	 return d+e;
+ }
+ public static float add(float n1,float n2)
+ {
+	 return n1+n2;
+ }
+ public static double multiply(double d,double e)
+ {
+	 return d*e;
+ }
+ public static double multiply(double n1,int n2)
+ {
+	 return n1*n2;
+ }
+ public static int multiply(int n1,int n2)
+ {
+	 return n1*n2;
+ }
+ public static int divide(int n1,int n2)
+ {
+	 return n1/n2;
+ }
+ public static double divide(double n1,int n2)
+ {
+	 return n1/n2;
+ }
+ public static double divide(double n1,double n2)
+ {
+	 return n1/n2;
+ }
+ public static double subtract(double n1,int n2)
+ {
+	 return n1-n2;
+ }
+ public static double subtract(double n1,double n2)
+ {
+	 return n1-n2;
+ }
+ public static int subtract(int n1,int n2)
+ {
+	 return n1-n2;
+ }
+ public static double subtract(int n1,double n2)
+ {
+	 return n1-n2;
+ }
+ 
+}
