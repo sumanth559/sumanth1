0e8373b56d6ffba8afba7b2f09fa9f6fa626076c
Sumanth <gopivenkata200@gmail.com>
2020-02-11 19:38:10 +0530

    Main.java added to repostrie

---
+++ Main.java
@@ -0,0 +1,19 @@
+package sample_calculator;
+
+public class Main {
+	public static void main(String args[])
+	{
+		System.out.println("addition of 10 and 19 is "+Math_operations.add(10,19));
+		System.out.println("addition of 10.0 and 19.0 is "+Math_operations.add(10.0,19.0));
+		System.out.println("addition of 10.0 and 19 is "+Math_operations.add(10.0,19));
+		System.out.println("multiplication  of 10 and 19 is "+Math_operations.multiply(10,19));
+		System.out.println("multiplication of 10.0 and 19 is "+Math_operations.multiply(10.0,19));
+		System.out.println("multiplication  of 10.0 and 19.0 is "+Math_operations.multiply(10.0,19.0));
+		System.out.println("division  of 10 and 19 is "+Math_operations.divide(10,19));
+		System.out.println("division of 10.0 and 19 is "+Math_operations.divide(10.0,19));
+		System.out.println("division of 10.0 and 19.0 is "+Math_operations.divide(10,19));
+		System.out.println("subtraction of 10 and 19 is "+Math_operations.subtract(10,19));
+		System.out.println("subtraction of 10.0 and 19.0 is "+Math_operations.subtract(10.0,19.0));
+		System.out.println("subtraction of 10.0 and 19 is "+Math_operations.subtract(10.0,19));
+	}
+}
